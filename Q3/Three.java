5624cce8bdeb4c28a3be7f83e4cfd9607060b6fe
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-03-16 05:11:15 +0000

    Q3 done

---
+++ Q3/Three.java
@@ -0,0 +1,21 @@
+public class Three {
+    public static void main(String args[]) {
+        for(int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@Kasunchira ➜ /workspaces/Practical-day-01/Q3 (main) $ java Three
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
