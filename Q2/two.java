4b052cd926ce955fc7b7723e4e7cf6d4a0cd5319
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-03-16 05:01:43 +0000

    Q2 done

---
+++ Q2/two.java
@@ -0,0 +1,22 @@
+public class two{
+    public static void main(String args[]){
+
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@Kasunchira ➜ /workspaces/Practical-day-01/Q2 (main) $ java two
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
