248848a93afcee0056facb13dc2fd2d21caa0b1f
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-03-16 04:52:42 +0000

    Question 1 done

---
+++ Q1/one.java
@@ -0,0 +1,10 @@
+//Print a mesage
+public class one{
+    public static void main( String args[]){
+        System.out.println("Hello World!");
+    }
+}
+
+
+//@Kasunchira ➜ /workspaces/Practical-day-01/Q1 (main) $ java one
+//Hello World!
