e7e06ccebd75139c3ece51a0324d5c59b9e84f1d
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-03-16 05:14:04 +0000

    Q4 done

---
+++ Q4/four.java
@@ -0,0 +1,24 @@
+
+
+public class four {
+    public static void main(String args[]){
+
+        for(int i=1; i<=20; i+=2){
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@Kasunchira ➜ /workspaces/Practical-day-01/Q4 (main) $ java four
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+*/
